423e61d18c4e30208ce04e5b37b70d6097ac158a
suhas <suhas.seeni@gmail.com>
2019-10-19 21:56:09 -0500

    add models, make migrations, add foodhacks as recognized app

--- demonhacks_project/demonhacks_project/settings.py
+++ demonhacks_project/demonhacks_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'foodhacks.apps.FoodhacksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- demonhacks_project/foodhacks/models.py
+++ demonhacks_project/foodhacks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Source(models.Model):
+    name = models.CharField(max_length = 30)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    desc = models.TextField()
+
+class Dest(models.Model):
+    name = models.CharField(max_length = 30)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    desc = models.TextField()
+    is_accepting_donations = models.PositiveSmallIntegerField()
+    requirement = models.PositiveSmallIntegerField()
+
+class Resources(models.Model):
+    rec_source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    exp_date = models.DateTimeField()
+    will_handle_delivery = models.PositiveSmallIntegerField()
+    weight = models.PositiveSmallIntegerField()
